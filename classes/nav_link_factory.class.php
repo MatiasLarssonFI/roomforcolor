687809962d96767b2509f38dc3381f3fe5fdb970
Matias Larsson <matias.larsson@gmail.com>
2016-06-21 21:14:10 +0300

    Continued transformation from woodparts

--- classes/nav_link_factory.class.php
+++ classes/nav_link_factory.class.php
@@ -1,13 +1,14 @@
 <?php
 
-require_once(dirname(__FILE__) . "/iwidget.class.php");
-require_once(dirname(__FILE__) . "/site_config_factory.class.php");
+require_once(dirname(__FILE__) . "/nav_link.class.php");
+require_once(dirname(__FILE__) . "/lang_link.class.php");
 require_once(dirname(__FILE__) . "/ui_text_storage.class.php");
 
 
 class NavLinkFactory {
     private $_action;
     private $_all_actions;
+    private $_lang;
     
     
     /**
@@ -18,11 +19,10 @@ class NavLinkFactory {
      * @param string $lang
      * @param string $base_uri
      */
-    public function __construct($action, array $nav_actions, $lang, $base_uri) {
+    public function __construct($action, array $nav_actions, $lang) {
         $this->_action = $action;
         $this->_all_actions = $nav_actions;
         $this->_lang = $lang;
-        $this->_base_uri = $base_uri;
     }
     
     
@@ -37,7 +37,6 @@ class NavLinkFactory {
             } else {
                 $title = $texts->text("NAV_FRONT_PAGE");
             }
-            $css_attr = ($action === $current_action ? "class='active'" : "");
             return new NavLink($action, $title, $action === $current_action);
         }, $this->_all_actions);
     }
@@ -45,9 +44,8 @@ class NavLinkFactory {
     
     public function get_lang_links() {
         $action = $this->_action;
-        $base_uri = $this->_base_uri;
         $lang = $this->_lang;
-        return array_map(function($lang) use ($base_uri, $action) {
+        return array_map(function($lang) use ($action) {
             return new LangLink($lang, strtoupper($lang), $action);
         }, array_filter(DBIF::get()->get_language_codes(), function($code) use ($lang) {
             return $code !== $lang;
