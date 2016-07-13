96a0ccb7354aae25119f30311f77185a32971e10
Matias Larsson <matias.larsson@gmail.com>
2016-07-13 21:52:35 +0300

    Image stage layout

--- classes/views/gallery_view.class.php
+++ classes/views/gallery_view.class.php
@@ -19,12 +19,17 @@ class GalleryView extends AbstractView {
     
     protected function get_view_data(array $params) {
         $factory = \GalleryFactory::get();
+        $text_storage = \UITextStorage::get();
         $gallery = $factory->get_gallery((int)$params["gallery_id"]);
+        $images = $factory->get_gallery_images($gallery->get_id());
         return array(
             "strings" => array(
-                "page_title" => $gallery->get_name()
+                "page_title" => $gallery->get_name(),
+                "next" => $text_storage->text("GALLERY_NEXT"),
+                "previous" => $text_storage->text("GALLERY_PREVIOUS"),
             ),
-            "images" => $factory->get_gallery_images($gallery->get_id())
+            "images" => $images,
+            "default_stage_image_url" => $this->get_stage_image_url($images)
         );
     }
     
@@ -34,5 +39,10 @@ class GalleryView extends AbstractView {
         return [
             "no_more_images" => $text_storage->text("GALLERY_NO_MORE_IMAGES")
         ];
+    }
+    
+    
+    private function get_stage_image_url(array $images) {
+        return (isset($images[0]) ? $images[0]->get_original_url() : "");
     }
 }
