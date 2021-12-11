da04c875d695d1394326173e3f9c02f3374b6acf
MatiasLarssonFI <matias.larsson@gmail.com>
2021-12-11 20:44:30 +0100

    Update PHPMailer
    Also, update code to be more like in the other code base.

--- classes/contact_message_mailer.class.php
+++ classes/contact_message_mailer.class.php
@@ -5,26 +5,48 @@ require_once(__DIR__ . "/imailer.class.php");
 require_once(__DIR__ . "/dbif.class.php");
 require_once(__DIR__ . "/site_config_factory.class.php");
 
-require_once(__DIR__ . "/../lib/PHPMailer-5.2.14/PHPMailerAutoload.php");
 require_once(__DIR__ . "/../lib_autoload.php");
+require_once(__DIR__ . "/../lib/PHPMailer-6.1.7/src/Exception.php");
+require_once(__DIR__ . "/../lib/PHPMailer-6.1.7/src/PHPMailer.php");
+require_once(__DIR__ . "/../lib/PHPMailer-6.1.7/src/SMTP.php");
+
+use PHPMailer\PHPMailer\PHPMailer;
+
 
 class ContactMessageMailer implements IMailer {
+    private $_mail;
+    
     public function send(IEmailMessage $contactmsg) {
+        try {
+            $this->_send($contactmsg);
+        } catch (\Exception $e) {
+            $dt = date("Y-m-d H:i:s");
+            file_put_contents(__DIR__ . "/../contact_email_error_log", "[{$dt}] {$e->getMessage()} - Mail error: {$this->_mail->ErrorInfo}" . PHP_EOL, FILE_APPEND);
+            throw $e;
+        }
+    }
+    
+    private function _send(IEmailMessage $contactmsg) {
         $mail = new PHPMailer;
+        $this->_mail = $mail;
+        
+        $sc = \SiteConfigFactory::get()->get_site_config();
         
         $loader = new \Twig\Loader\FilesystemLoader(__DIR__ . "/../templates");
         $twig = new \Twig\Environment($loader, [
             "cache" => __DIR__ . "/../../twig_compilation_cache",
         ]);
         
-        $host = \SiteConfigFactory::get()->get_site_config()->host();
+        $host = $sc->host();
         $db = \DBIF::get();
 
         $mail_user = $db->get_mail_user();
-        $mail_password = $db->get_mail_password();
-        if (strlen($mail_user) > 0) { // username can also just indicate SMTP, lol
-            $mail->isSMTP(); // Set mailer to use SMTP
+        
+        if (strlen($mail_user) > 0) { // username can also just indicate SMTP
+            $mail->isSMTP();
             $mail->Host = $db->get_mail_server();
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+            $mail_password = $db->get_mail_password();
             if (strlen($mail_password) > 0) {
                 $mail->SMTPAuth = true;
                 $mail->Username = $mail_user;
@@ -39,18 +61,17 @@ class ContactMessageMailer implements IMailer {
         
         $mail->addReplyTo($contactmsg->get_email(), $contactmsg->get_name());
         $mail->FromName ='Room for color Contact Form';
-        $mail->From = "contactform@{$host}";
+        $mail->From = "contactform@roomforcolor.fi";
         $mail->addAddress($db->get_contact_email());     // recipient
         $mail->isHTML(true);                             // email format to HTML
     
-        $mail->CharSet = 'UTF-8';
+        $mail->CharSet = PHPMailer::CHARSET_UTF8;
         $mail->Subject = "RFC-CONTACT: {$contactmsg->get_subject()}";
         $mail->Body    = $html_tmpl->render([ "message" => $contactmsg ]);
         $mail->AltBody = $text_tmpl->render([ "message" => $contactmsg ]);
 
         if(!$mail->send()) {
-            file_put_contents(__DIR__ . "/../contact_email_error_log", "{$mail->ErrorInfo}\n", FILE_APPEND);
-            throw new \RuntimeException("Failed to send contact mail. Mail error: {$mail->ErrorInfo}");
+            throw new \RuntimeException("Failed to send contact form e-mail.");
         }
     }
 }
